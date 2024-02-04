dd705ce83a9ee95ed8618074d9b00d3676c332b1
Masoud Maghsoudi <masoud_maghsoudi@yahoo.com>
2024-02-04 22:07:08 +0330

    finished employee management project

--- quera-bash-chanllenges/employee-management/manager.sh
+++ quera-bash-chanllenges/employee-management/manager.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#check wether command is valid or not
 if [[ ! ($1 = 'bonus' || $1 = 'city') ]]; then
     echo "command not found"
 fi
@@ -7,21 +8,27 @@ fi
 file="./employee.csv"
 declare -a emp_arr
 
-mapfile -d ',' -t emp_arr <./employee.csv
+#parse customer records in array
+while read -r line; do
+    emp_arr+=("$line")
+done <"$file"
 
-declare -p emp_arr
-
+#implement commands
 case "$1" in
 bonus)
-    #echo "bonus"
-    for i in $(seq $(wc -l $file)); do
-        if [[ $emp_arr[$i, 0] = $2]]; then
-            echo "$emp_arr[$i, 2] will get \$(($emp_arr[$i, 4]))"
+    for record in "${emp_arr[@]}"; do
+        if [[ $(echo "$record" | cut -d ',' -f 1) = "$2" ]]; then
+            echo "$(echo "$record" | cut -d ',' -f 3) will get \$$(($(echo "$record" | cut -d ',' -f 5) * 5 / 100)) bonus"
         fi
     done
     ;;
-#city)
-#    echo "city"
-#    ;;
-#esac
-#
+city)
+    for record in "${emp_arr[@]}"; do
+        if [[ $(echo "$record" | cut -d ',' -f 2) = "$2" ]]; then
+            echo "Customer Name: $(echo "$record" | cut -d ',' -f 3)"
+            echo "Mobile No: $(echo "$record" | cut -d ',' -f 4)"
+
+        fi
+    done
+    ;;
+esac
