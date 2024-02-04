614779134cfd0acfbbc18dd30d4e1d2e3e90750f
Masoud Maghsoudi <masoud_maghsoudi@yahoo.com>
2024-02-05 00:19:27 +0330

    finished busy Salib challenge

---
+++ quera-bash-chanllenges/busy_Salib/README.md
@@ -0,0 +1,5 @@
+# code challenge: busy Salib
+
+In order to meet the challenge of [employee management](https://quera.org/problemset/129783), this script makes a personal calendar for a guy named "Salib" to organize his daily meetings.
+
+More detailed descriptions found on [project description](https://quera-org.translate.goog/problemset/129783?tab=description&_x_tr_sl=auto&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp&_x_tr_hist=true).
--- quera-bash-chanllenges/busy_Salib/checker.sh
+++ quera-bash-chanllenges/busy_Salib/checker.sh
@@ -1,22 +1,54 @@
 #!/bin/bash
 
-#check wether command is valid or not
-if [[ ! ($1 = 'add' || $1 = 'check') || ($1 = 'free' || $1 = 'exit') ]]; then
-    echo "Command not found."
-fi
+hours=()
 
-case "$1" in
-exit)
-    echo "Bye Bye!"
-    exit
-    ;;
-add)
-    echo "add"
-    ;;
-check)
-    echo "add"
-    ;;
-free)
-    echo "free"
-    ;;
-esac
+for i in {0..23}; do
+    hours+=("free")
+    ((i++))
+done
+
+while read -r line; do
+    #split command , start and finish time from input
+    command=$(echo "$line" | tr -s ' ' | cut -d ' ' -f 1)
+    start=$(echo "$line" | tr -s ' ' | cut -d ' ' -f 2)
+    finish=$(echo "$line" | tr -s ' ' | cut -d ' ' -f 3)
+    #check wether command is valid or not
+    if [[ ! ($command = 'add' || $command = 'check' || $command = 'free' || $command = 'exit') ]]; then
+        echo "Command not found."
+        continue
+    fi
+
+    case "$command" in
+    exit)
+        echo "Bye Bye!"
+        exit
+        ;;
+    add)
+        for i in $(seq "$start" $(("$finish" - 1))); do
+            hours["$i"]="busy"
+        done
+        echo "A meeting from $start to $finish added to your day!"
+        ;;
+    check)
+        flag="free"
+        for i in $(seq "$start" $(("$finish" - 1))); do
+            if [[ ${hours["$i"]} = "free" ]]; then
+                continue
+            else
+                flag="busy"
+                echo "You can't have a meeting from $start to $finish!"
+                break
+            fi
+        done
+        if [ "$flag" = "free" ]; then
+            echo "You can add a meeting from $start to $finish!"
+        fi
+        ;;
+    free)
+        for i in $(seq "$start" $(("$finish" - 1))); do
+            hours["$i"]="free"
+        done
+        echo "You're now free from $start to $finish!"
+        ;;
+    esac
+done
