81466af75d91cdb30c6eb781cc7552da7c9b00a5
Masoud Maghsoudi <masoud_maghsoudi@yahoo.com>
2024-02-05 23:42:42 +0330

    init task manager challenge

---
+++ quera-bash-chanllenges/task_manager/task.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+task_file="task.txt"
+
+task_list() {
+    line_count=1
+    while read -r line; do
+        if [[ $(echo "$line" | cut -d ' ' -f 1) = "L" ]]; then
+            echo "$line_count ${line/"L"/"*"}"
+        elif [[ $(echo "$line" | cut -d ' ' -f 1) = "M" ]]; then
+            echo "$line_count ${line/"M"/"***"}"
+        else
+            echo "$line_count ${line/"H"/"*****"}"
+        fi
+        ((line_count++))
+    done <$task_file
+}
+
+#check wether command is valid or not
+if [[ ! ($1 = 'add' || $1 = 'list' || $1 = 'done') ]]; then
+    echo "[Error] Invalid command"
+    exit 2
+fi
+
+#check wether command argument is valid or not
+if [[ (($1 = 'add' || $1 = 'done')) && ($# -eq 1) ]]; then
+    echo "[Error] This command needs an argument"
+    exit 2
+fi
+
+case $1 in
+add)
+    echo "add"
+    ;;
+list)
+    echo "list"
+    if [[ (! -f $task_file) || ($(wc -l $task_file | cut -d ' ' -f 1) = 0) ]]; then
+        echo "No tasks found..."
+    else
+        task_list
+    fi
+    ;;
+done)
+    echo "done"
+    ;;
+esac
